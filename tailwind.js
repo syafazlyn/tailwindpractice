ef0dc013d253a7fdf74e37a7a02aaf3417b6631e
syarifah fazlyn <syafazlyn@gmail.com>
2019-08-15 13:01:46 +0800

    First commit

---
+++ tailwind.js
@@ -0,0 +1,35 @@
+module.exports = {
+  theme: {
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+}
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ]
+}
+
+ margin: {
+    'px': '1px',
+    '0': '0',
+    '1': '0.25rem',
+    '2': '0.5rem',
+    '3': '0.75rem',
+    '4': '1rem',
+    '6': '1.5rem',
+    '8': '2rem',
+  },
+  
+  
+  
+borderWidths: {
+    default: '1px',
+    '0': '0',
+    '2': '2px',
+    '4': '4px',
+    '8': '8px',
+    '12': '12px' // Add it here
+  },
